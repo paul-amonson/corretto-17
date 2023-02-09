a0f1d06a3c9e6d384a884e61f6a38ea0351a56c0
Paul Amonson <paul.d.amonson@intel.com>
2023-02-09 08:30:41 -0800

    [Patch] #7
    Signed-off-by: Paul Amonson <paul.d.amonson@intel.com>

--- src/hotspot/share/jvmci/vmStructs_jvmci.cpp
+++ src/hotspot/share/jvmci/vmStructs_jvmci.cpp
@@ -309,6 +309,7 @@
   static_field(StubRoutines,                _electronicCodeBook_encryptAESCrypt,              address)                               \
   static_field(StubRoutines,                _electronicCodeBook_decryptAESCrypt,              address)                               \
   static_field(StubRoutines,                _counterMode_AESCrypt,                            address)                               \
+  static_field(StubRoutines,                _galoisCounterMode_AESCrypt,                      address)                               \
   static_field(StubRoutines,                _base64_encodeBlock,                              address)                               \
   static_field(StubRoutines,                _base64_decodeBlock,                              address)                               \
   static_field(StubRoutines,                _ghash_processBlocks,                             address)                               \
